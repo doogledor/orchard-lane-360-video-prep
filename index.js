42e3010fc88ba643e33a195e208b639fcfb01369
samelie <sam@stinkdigital.com>
2017-07-04 14:58:37 -0700

    uno

---
+++ index.js
@@ -0,0 +1,16 @@
+const DashUpload = require('video-dash-uploader')
+
+const spawn = require('child_process').spawnSync
+
+const PLAYLIST = "https://www.youtube.com/playlist\?list\=PLZRcgvIPIUuWH9WxuUqV2QEs5S-C63vzx"
+
+const child = spawn(`youtube-dl`, [
+  PLAYLIST,
+  "--flat-playlist",
+  "-J"
+])
+
+const stderr = child.stderr.toString('utf-8');
+const stdout = JSON.parse(child.stdout.toString('utf-8'));
+const IDS =  stdout.entries.map(e=>e.url)
+DashUpload(IDS,"gs://orchard-lane", {save:__dirname})
