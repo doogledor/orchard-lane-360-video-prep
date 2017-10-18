915dba6d4c95f3ddea18e6891f47e0a353c722c4
samelie <sam@stinkdigital.com>
2017-10-18 14:24:29 -0700

    Use latest versions

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const DashUpload = require('youtube-video-uploader')
+const DashUpload = require("youtube-video-uploader")
 
-const spawn = require('child_process').spawnSync
+const spawn = require("child_process").spawnSync
 
-const PLAYLIST = "https://www.youtube.com/playlist\?list\=PLZRcgvIPIUuWH9WxuUqV2QEs5S-C63vzx"
+const PLAYLIST =
+  "https://www.youtube.com/playlist?list=PLZRcgvIPIUuWH9WxuUqV2QEs5S-C63vzx"
 
-const child = spawn(`youtube-dl`, [
-  PLAYLIST,
-  "--flat-playlist",
-  "-J"
-])
+const child = spawn(`youtube-dl`, [PLAYLIST, "--flat-playlist", "-J"])
 
-const stderr = child.stderr.toString('utf-8');
-const stdout = JSON.parse(child.stdout.toString('utf-8'));
-console.log(stdout.entries);
-const IDS =  stdout.entries.map(e=>e.url)
-DashUpload(IDS,"gs://orchard-lane", {save:__dirname})
+const stderr = child.stderr.toString("utf-8")
+const stdout = JSON.parse(child.stdout.toString("utf-8"))
+console.log(stdout.entries)
+const IDS = stdout.entries.map(e => e.url)
+DashUpload(IDS, "gs://orchard-lane", { save: __dirname, itags:["266","264","137"] })
